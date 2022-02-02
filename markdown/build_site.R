c10569f8d9dbdb18a4f0f08b93efbbc5bc3893de
Chloe Jelley <cmj96@cornell.edu>
2022-02-01 19:23:07 -0500

    Added instructions for an auto commiting script

--- markdown/build_site.R
+++ markdown/build_site.R
@@ -3,3 +3,20 @@ setwd("~/Documents/website/markdown")
 
 #render the site
 rmarkdown::render_site()
+
+# To make changes live:
+# Save everything
+# Go to Terminal
+# Run this command:
+## ./publish
+# It will ask for a commit message
+# Enter a short description of the changes you made (very short)
+
+
+# If that doesn't work, try this:
+
+# Make sure the working directory is correct:
+## cd ~/Documents/website
+## git add .
+## git commit -m "Some text saying what you did"
+## git push
